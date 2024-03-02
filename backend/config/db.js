04c20c8b8fdd401165d96b8dba459e1f97bfc102
gautamjaiswal22 <jaiswalgautam252@gmail.com>
2024-03-02 22:37:20 +0530

    socket added

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,17 +1,30 @@
+// const mongoose = require('mongoose');
+// // const { MongoClient, ServerApiVersion } = require('mongodb');
+// require("dotenv").config();
+// const url1 = process.env.DB_URL;
+// // mongoose.connect(uri);
+// mongoose.connect(url1, { useNewUrlParser: true, useUnifiedTopology: true });
+// const connection = mongoose.connection;
+// // const connection = mongoose.createConnection(uri);     
+// connection.on('open', () => {
+//     console.log("MongoDB connected with blacksparrow yoyo");
+// }).on('error', (error) => {
+//     console.log("MongoDB connection error: " + error);
+// });
+// module.exports = connection;
+
 const mongoose = require('mongoose');
-const { MongoClient, ServerApiVersion } = require('mongodb');
 require("dotenv").config();
-const url1 = process.env.DB_URL;
-// mongoose.connect(uri);
-mongoose.connect(url1, { useNewUrlParser: true, useUnifiedTopology: true });
-
+// Retrieve MongoDB connection URL from environment variables
+const uri = process.env.DB_URL;
+// Connect to MongoDB using Mongoose
+mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 const connection = mongoose.connection;
-// const connection = mongoose.createConnection(uri);     
-connection.on('open', () => { 
-    console.log("MongoDB connected with blacksparrow yoyo");
-}).on('error', (error) => {
-    console.log("MongoDB connection error: " + error);
+// Event listeners to check for successful connection or errors
+connection.on('connected', () => {
+    console.log('MongoDB connected with blacksparrow yoyo');
 });
-
+connection.on('error', (error) => {
+    console.log('MongoDB connection error:', error);
+});
 module.exports = connection;
-
